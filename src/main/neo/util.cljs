d21624281a6834531fb93e595a829fc3c842ee7a
Patrick Killean <pat.killean@gmail.com>
2018-06-17 21:02:30 -0400

    *eps* fix

--- src/main/neo/util.cljs
+++ src/main/neo/util.cljs
@@ -1,6 +1,7 @@
 (ns neo.util)
 
 (defn map!
+  "maps f or second arg array, mutating in place"
   [f arr]
   (assert (goog.isArrayLike arr))
   (let [stop (alength arr)]
@@ -18,7 +19,7 @@
         (aset arr i (f i (aget arr i)))
         (recur (inc i))))))
 
-(def ^:dynamic *eps* 1e10)
+(def ^:dynamic *eps* 1e-10)
 
 (defn ^number epsilon [n]
   (if (< (js/Math.abs n) *eps*)
