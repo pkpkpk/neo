8600600ca67181e1fbbc31c5845938327da8682b
Patrick Killean <pat.killean@gmail.com>
2018-10-13 18:58:13 -0400

    0.1.0

--- project.clj
+++ project.clj
@@ -1,9 +1,12 @@
-(defproject neo "0.0.1"
-
+(defproject neo "0.1.0"
+  :description "A DOM animation lib for clojurescript"
+  :url "https://github.com/pkpkpk/neo"
+  :repositories [["clojars" {:sign-releases false}]]
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.9.0"]
-                 [org.clojure/clojurescript "1.10.238"]
-                 [org.clojure/core.async "0.4.474"]
-                 [org.clojure/tools.reader "1.2.1"]]
+                 [org.clojure/clojurescript "1.10.339"]
+                 [org.clojure/core.async "0.4.474"]]
 
   :plugins [[lein-cljsbuild "1.1.7"]]
 
@@ -29,5 +32,6 @@
                        :source-paths ["src" "examples/periodic/src"]
                        :compiler {:main periodic.core
                                   :asset-path "js/compiled/out"
+                                  :optimizations :advanced
                                   :output-to  "examples/periodic/resources/public/js/compiled/periodic.js"
                                   :output-dir "examples/periodic/resources/public/js/compiled/out"}}]}}})
